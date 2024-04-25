9010e91e5a42879e676d357fa5dd1494e57f43fb
salient-sss <107886171+salient-sss@users.noreply.github.com>
2024-04-25 11:29:05 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,8 +43,8 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode} from "@vicons/fa"; // 注意使用正确的类别
-import { MovieCreationSharp } from "@vicons/material"
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode ,Video} from "@vicons/fa"; // 注意使用正确的类别
+// import { MovieCreationSharp } from "@vicons/material"
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -71,7 +71,7 @@ const siteIcon = {
   Book,
   Fire,
   LaptopCode,
-  MapperScannerConfigure,
+  Video,
 };
 
 // 链接跳转
